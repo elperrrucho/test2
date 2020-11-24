d0702113876053fd3e880443d3d63b3c945561d7
Elrey <eddygarcia06@gmail.com>
2020-11-24 16:29:58 -0500

    nouvelle fonction

---
+++ fonction.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+	
+
+	maFonction() {
+		echo "mon parametre a la valeur $1, et lautre la valeur $2"
+	}
+	maFonction $1 $2
+	# res mon parametre a la valeur pirate, et lautre la valeur poutine
+	
+
+	#afficherInfo() {
+	#	echo "-------------------------------"
+	#	echo "kernel : " `uname -rs`
+	#	echo "-------------------------------"
+	#}
+	#appel une fonction sans parametres
+	#afficherInfo
+
